9b95fb2cac71606ad92bd47c09fc5027e9bc1153
Mr Christian <feyemlionel@gmail.com>
2024-11-27 18:09:49 +0100

    all

--- FrontEnd/src/routes/auth_route.jsx
+++ FrontEnd/src/routes/auth_route.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Auth/Login';
-
+import Regist from '../pages/Auth/Regist';
 const AuthRoute = () => {
     return (
         
             <Routes>
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Regist />} />
             </Routes>
         
     )
