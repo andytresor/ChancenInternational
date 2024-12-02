c8313d13944f127c67ebf82654207ca491da4e93
Nyuykighan Jane <nyuykighanjane@gmail.com>
2024-12-02 18:03:35 +0100

    Landing and 404 page

--- FrontEnd/src/Landing-Page/landing.jsx
+++ FrontEnd/src/Landing-Page/landing.jsx
@@ -1,5 +1,6 @@
 import logo from "../assets/Images/adminImages/Chance.png";
 import AboutUs from "../assets/Images/AboutUs.svg";
+import steps from "../assets/Images/steps.svg";
 import { Link } from "react-router-dom";
 import "./landing.css";
 
@@ -19,7 +20,7 @@ const LandingPage = () => {
             <ul>
               <li>About Us</li>
               <li>Services</li>
-              <Link>
+              <Link to="/auth/register">
                 <button>Register</button>
               </Link>
             </ul>
@@ -27,7 +28,7 @@ const LandingPage = () => {
         </div>
         <div className="welcome">
           <div className="text">
-            <h1>Investing in Futures Through Education.</h1>
+            <h1>Investing in Futures Through <br />Education.</h1>
             <p>
               We fund your education so you can pay it forward when you succeed.
             </p>
@@ -37,6 +38,7 @@ const LandingPage = () => {
         <div className="about-us">
           <img src={AboutUs} alt="about us" />
           <div className="part">
+            <h1>  About Us</h1>
             <p>
               Chancen provides access to quality education that equips you for
               the modern workforce. With Income Share Agreement we invest in
@@ -55,16 +57,33 @@ const LandingPage = () => {
 
         <div className="working">
           <div className="part2">
+            <h1>How It Works</h1>
+            <p>- Apply for Funding: Submit your application.</p>
             <p>
-            Apply for Funding: Submit your application.</p>
-         <p></p>  Get Funded: Receive financial support for tuition and living expenses.
-           Graduate & Start Your Career: Achieve your goals with our support.
-           Repay a Small Percentage: Help others like you by repaying 15% of your salary.
-            
+              - Get Funded: Receive financial support for tuition and living
+              expenses.
+            </p>
+            <p>
+             -  Graduate & Start Your Career: Achieve your goals with our support.
+            </p>
+            <p>
+              - Repay a Small Percentage: Help others like you by repaying 15% of
+              your salary.
+            </p>
           </div>
+          <img src={steps} alt="steps" />
         </div>
 
-
+        <div className="footer">
+          <footer>
+            <ul>
+              <li>About Us</li>
+              <li>Services</li>
+            </ul>
+            <hr />
+            <p>Copyright © 2024 Chancen Cameroon International.</p>
+          </footer>
+        </div>
       </div>
     </>
   );
